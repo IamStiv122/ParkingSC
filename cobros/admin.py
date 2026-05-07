04e880db9224bff5bb11fdc506a9cc87853349dc
steven <stevenson.celi6741@utc.edu.ec>
2026-05-07 14:43:53 -0500

    Correcion de errores

--- cobros/admin.py
+++ cobros/admin.py
@@ -4,4 +4,5 @@ from .models import Cliente, Vehiculo, EspacioEstacionamiento, Cobro
 admin.site.register(Cliente)
 admin.site.register(Vehiculo)
 admin.site.register(EspacioEstacionamiento)
+admin.site.register(TarifaHora)
 admin.site.register(Cobro)
